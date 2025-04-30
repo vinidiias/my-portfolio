65df7acd6874039664fb6b7227521fe638b2e870
vinidiias <vininoites8350@gmail.com>
2025-04-29 22:49:56 -0300

    feat: responsive on mobile finalized

--- src/components/ui/project_dialog.tsx
+++ src/components/ui/project_dialog.tsx
@@ -130,11 +130,6 @@ export const ProjectDialog: React.FC<ProjectDialogProps> = ({ open, handleClose,
             </div>
           </div>
         </DrawerHeader>
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
