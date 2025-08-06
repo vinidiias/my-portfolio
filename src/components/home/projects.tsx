6c1a976fb7a047e2c0e76a846ff67e1e649bb6cc
Vinícius de Oliveira Dias <vinicius.dias@transp.net>
2025-08-06 11:33:34 -0300

    add new project

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -1,3 +1,4 @@
+// COMPONENTS
 import {
   Card,
   CardContent,
@@ -5,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+// LOGOS
 import html from "@/assets/html.svg";
 import css from "@/assets/css.svg";
 import js from "@/assets/js.svg";
@@ -13,19 +15,26 @@ import tailwindcss from "@/assets/tailwindcss.svg";
 import bootstrap from "@/assets/bootstrap.svg";
 import query from "@/assets/query.svg";
 import jotai from "@/assets/jotai.svg";
+import jotai_black from "@/assets/jotai_black.svg";
 import material_ui from "@/assets/material-ui.svg";
 import node from "@/assets/node.svg";
 import mongoDB from "@/assets/mongoDB.svg";
 import docker from "@/assets/docker.svg";
+import next_white from "@/assets/next_white.svg";
+import next_black from "@/assets/next_black.svg";
+import shadcn from "@/assets/shadcn-ui.svg";
+// SCREENSHOTS
 import cw from "../../assets/screenshots/cw-screenshot.png";
-import todo_list from "../../assets/screenshots/todo-list-screenshot.png";
-import e_market from "../../assets/screenshots/emarket-screenshot.png";
-import evento from "../../assets/screenshots/eventos-screenshots.png";
-import croche from "../../assets/screenshots/croche-screenshot.png";
-import netflix from "../../assets/screenshots/netflix-screenshot.png";
-import social_media from "../../assets/screenshots/social-media-screenshot.png";
-import smart_soft from "../../assets/screenshots/smart-soft-screenshot.png";
-import recipe from "../../assets/screenshots/recipe-screenshot.png";
+import todo_list from "@/assets/screenshots/todo-list-screenshot.png";
+import e_market from "@/assets/screenshots/emarket-screenshot.png";
+import evento from "@/assets/screenshots/eventos-screenshots.png";
+import croche from "@/assets/screenshots/croche-screenshot.png";
+import netflix from "@/assets/screenshots/netflix-screenshot.png";
+import social_media from "@/assets/screenshots/social-media-screenshot.png";
+import smart_soft from "@/assets/screenshots/smart-soft-screenshot.png";
+import recipe from "@/assets/screenshots/recipe-screenshot.png";
+import ts from "@/assets/ts.svg";
+import smoke_burguer from "../../assets/screenshots/smoke-burguer-screenshot.png"
 import { VscDebugStart } from "react-icons/vsc";
 import { FaGithub } from "react-icons/fa";
 import { AspectRatio } from "../ui/aspect-ratio";
@@ -35,12 +44,13 @@ import { ProjectDialog } from "../ui/project_dialog";
 import { ProjecInfo } from "@/types/project";
 import { UseMediaQuery } from "@/hooks/use_media_query";
 import { useIntl } from "react-intl";
+import { useTheme } from "@/hooks/useTheme";
 
 export const Projects = () => {
-  const [projectselected, setProjectSelected] = useState<ProjecInfo | null>(
-    null
-  );
+  const [projectselected, setProjectSelected] = useState<ProjecInfo | null>(null);
   const [open, setOpen] = useState(false);
+
+  const { theme } = useTheme();
 
   const intl = useIntl();
 
@@ -62,7 +72,7 @@ export const Projects = () => {
       material_ui,
       tailwindcss,
       query,
-      jotai,
+      theme === 'dark' ? jotai : jotai_black,
       node,
       mongoDB,
       docker,
@@ -71,77 +81,92 @@ export const Projects = () => {
   },
   {
     title: "projects2.title",
+    photo: smoke_burguer,
+    link: "https://smoke-burguer-2ywq.vercel.app/",
+    github: "https://github.com/vinidiias/smoke-burguer",
+    tech: [
+      react,
+      ts,
+      theme === 'dark' ? next_white : next_black,
+      shadcn,
+      tailwindcss,
+      theme === 'dark' ? jotai : jotai_black,
+    ],
+    about: 'projects2.about',
+  },
+  {
+    title: "projects3.title",
     photo: todo_list,
     link: "https://todo-list-omega-sooty.vercel.app/login",
     github: "https://github.com/vinidiias/todo-list",
     tech: [react, node, mongoDB],
     about:
-      "projects2.about",
+      "projects3.about",
   },
   {
-    title: "projects3.title",
+    title: "projects4.title",
     photo: e_market,
     link: "https://e-market-iota.vercel.app",
     github: "https://github.com/vinidiias/e-market",
     tech: [html, css, js, bootstrap],
     about:
-      "projects3.about",
+      "projects4.about",
   },
   {
-    title: "projects4.title",
+    title: "projects5.title",
     photo: evento,
     link: "https://sistema-gestao-de-eventos.vercel.app/login",
     github: "https://github.com/vinidiias/sistema-gestao-de-eventos",
     tech: [react],
     about:
-      "projects4.about",
+      "projects5.about",
   },
   {
-    title: "projects5.title",
+    title: "projects6.title",
     photo: croche,
     link: "https://catalogo-croche.vercel.app",
     github: "https://github.com/vinidiias/catalogo_croche",
     tech: [react, node, mongoDB],
     about:
-      "projects5.about",
-  },
-  {
-    title: "projects6.title",
-    photo: netflix,
-    link: "https://netfix-mylove.vercel.app",
-    github: "https://github.com/vinidiias/netfix_girlfriend",
-    tech: [react],
-    about:
       "projects6.about",
   },
   {
-    title: "projects7.title",
-    photo: social_media,
-    link: "https://social-media-react-seven.vercel.app",
-    github: "https://github.com/vinidiias/social-media-react",
+    title: "projects67.title",
+    photo: netflix,
+    link: "https://netfix-mylove.vercel.app",
+    github: "https://github.com/vinidiias/netfix_girlfriend",
     tech: [react],
     about:
       "projects7.about",
   },
   {
     title: "projects8.title",
-    photo: smart_soft,
-    link: "https://login-screen-smart-soft.vercel.app",
-    github: "https://github.com/vinidiias/login-screen-smart-soft",
-    tech: [html, css, js],
+    photo: social_media,
+    link: "https://social-media-react-seven.vercel.app",
+    github: "https://github.com/vinidiias/social-media-react",
+    tech: [react],
     about:
       "projects8.about",
   },
   {
     title: "projects9.title",
+    photo: smart_soft,
+    link: "https://login-screen-smart-soft.vercel.app",
+    github: "https://github.com/vinidiias/login-screen-smart-soft",
+    tech: [html, css, js],
+    about:
+      "projects9.about",
+  },
+  {
+    title: "projects10.title",
     photo: recipe,
     link: "https://recipe-page-main-eosin.vercel.app",
     github: "https://github.com/vinidiias/recipe-page-main",
     tech: [html, css],
     about:
-      "projects9.about",
+      "projects10.about",
   },
-], [])
+], [theme])
 
   return (
     <section className="w-full" id="projects">
